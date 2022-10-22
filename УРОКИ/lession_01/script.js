e6c7a52312ea4c9de66e9439b7e3eee94a5ea028
Geran1ka <roman.khoruzhy@gmail.com>
2022-10-22 19:46:46 +0300

    initial

---
+++ УРОКИ/lession_01/script.js
@@ -0,0 +1,9 @@
+const nameProduct = 'Bugatti Vieron';
+console.log('nameProduct: ', nameProduct);
+const quantityProduct = 5;
+const categoryProduct = 'Auto';
+const priceProduct = 100_000;
+
+const priceProductAll = quantityProduct * priceProduct;
+console.log('priceProductAll: ', priceProductAll);
+
