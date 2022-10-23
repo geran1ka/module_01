43fb6831ccaad88a65ea31a128cb7ca5e75eedc7
Geran1ka <roman.khoruzhy@gmail.com>
2022-10-23 07:45:07 +0300

    Добавлен второй товар

--- УРОКИ/lession_01/script.js
+++ УРОКИ/lession_01/script.js
@@ -6,3 +6,12 @@ const price = 100_000;
 
 console.log(item);
 console.log(`общая сумма товара: ${count * price}`);
+
+const item2 = 'HP Pavilion DV6';
+
+const count2 = 15;
+const category2 = 'Notebook';
+const price2 = 10_000;
+
+console.log(item2);
+console.log(`общая сумма товара: ${count2 * price2}`);
