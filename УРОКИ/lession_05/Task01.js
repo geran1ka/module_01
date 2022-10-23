52d610a182bd65468410ee6c5dc6c15ef637fa64
Geran1ka <roman.khoruzhy@gmail.com>
2022-10-23 18:55:17 +0300

    Задача №1 урок 5

---
+++ УРОКИ/lession_05/Task01.js
@@ -0,0 +1,7 @@
+'use strict'
+//Задача №1 урок 5
+const convertCurrency = (currency) => {
+    const result = currency *1.2 * 73
+    return result
+}
+console.log('convertCurrency(10): ', convertCurrency(10));
